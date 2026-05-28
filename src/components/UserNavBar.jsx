1385a0f1647ddbbe9834f961d812404923c2335d
Ong Aik Ngee <73453714+ongaikngee@users.noreply.github.com>
2026-05-28 21:32:38 +0800

    position UserNavBar icon with label

--- src/components/UserNavBar.jsx
+++ src/components/UserNavBar.jsx
@@ -1,19 +1,23 @@
 import React from 'react'
 import { NavLink } from "react-router-dom";
-import {PokerChipIcon, GearIcon} from "@phosphor-icons/react";
+import { PokerChipIcon, GearIcon } from "@phosphor-icons/react";
 
 export const UserNavBar = () => {
 	return (
 		<div className="col-12 bg-secondary py-3">
 			<nav className="nav">
 				<NavLink to="chips" className="nav-link text-white">
-					<PokerChipIcon size={24} />
-					Chips
+					<div className="d-flex flex-column align-items-center">
+						<PokerChipIcon size={40} />
+						<div className="mb-0">Chips</div>
+					</div>
 				</NavLink>
 
 				<NavLink to="settings" className="nav-link text-white">
-					<GearIcon size={24} />
-					Settings
+					<div className="d-flex flex-column align-items-center">
+						<GearIcon size={40} />
+						<div className="mb-0">Settings</div>
+					</div>
 				</NavLink>
 			</nav>
 		</div>
